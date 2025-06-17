c5ec0c5ed7b5d071fb20c6382d42ec6a7dffc54b
JadeM22 <jade.miralda@unah.hn>
2025-06-17 02:43:26 -0600

    Update test_operaciones_comparacion.py

--- test_operaciones_comparacion.py
+++ test_operaciones_comparacion.py
@@ -8,13 +8,11 @@ class TestOperacionesComparacion(unittest.TestCase):
 
     def test_es_mayor_que(self):
         self.assertTrue(es_mayor_que(10, 5))
-        self.assertFalse(es_mayor_que(2, 4))
         self.assertFalse(es_mayor_que(-6, 7))
 
     def test_es_menor_que(self):
         self.assertTrue(es_menor_que(2, 4))
         self.assertFalse(es_menor_que(5, 2))
-        self.assertFalse(es_menor_que(5, 5))
 
     def test_es_mayor_o_igual_que(self):
         self.assertTrue(es_mayor_o_igual_que(9, 3))
